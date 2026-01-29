431292a942898f0bd52d75fa5e4fa506a29cd732
imoygemoy <moyyprivate@gmail.com>
2026-01-29 15:41:50 +0700

    menambahkan request dan controller

---
+++ app/Http/Requests/category/category.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class Storecategory extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'category_name'=> [
+                'required',
+                'string',
+                'max:225',
+        'unique:categories.category_name'
+            ],
+        ];
+    }
+
+      public function messages(): array
+    {
+        return [
+            'category_name.required'=>'Nama kategori tidak boleh kosong.',
+            'category_name.unique'=>'Nama kategori sudah terdaftar.',
+            'category_name.max'=>'Nama kategori maksimal 255 karakter.',
+        ];
+    }
+}
+  
